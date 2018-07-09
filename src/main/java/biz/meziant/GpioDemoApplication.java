fdff9d67177197afeb4be309bc824606ac3e4789
Ronan GUILLAMET <ronan@guillamet.com>
2018-07-09 15:48:02 +0200

    First commit

---
+++ src/main/java/biz/meziant/GpioDemoApplication.java
@@ -0,0 +1,24 @@
+package biz.meziant;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class GpioDemoApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(GpioDemoApplication.class, args);
+
+		new Led().execute();
+
+	}
+}
